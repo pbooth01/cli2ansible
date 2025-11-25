52cf4e7e3c93b8ea5d93c3ee3c0501bfbe098dcb
Phillip Booth <phillip.booth2015@gmail.com>
2025-11-24 21:17:28 -0500

    removing prompt files

--- src/cli2ansible/application/dtos/__init__.py
+++ src/cli2ansible/application/dtos/__init__.py
@@ -1,19 +1,19 @@
 """Data Transfer Objects for application layer."""
 
-from cli2ansible.application.dtos.clean import (
+from cli2ansible.application.dtos.clean_dto import (
     CleanedCommandResponseDTO,
     CleaningReportResponseDTO,
     CleanRequestDTO,
     CleanSessionResponseDTO,
 )
-from cli2ansible.application.dtos.compile import CompileRequestDTO, CompileResponseDTO
-from cli2ansible.application.dtos.event import (
+from cli2ansible.application.dtos.compile_dto import CompileRequestDTO, CompileResponseDTO
+from cli2ansible.application.dtos.event_dto import (
     EventCreateRequestDTO,
     EventResponseDTO,
     EventUpdateRequestDTO,
 )
-from cli2ansible.application.dtos.report import ReportResponseDTO
-from cli2ansible.application.dtos.session import SessionCreateRequestDTO, SessionResponseDTO
+from cli2ansible.application.dtos.report_dto import ReportResponseDTO
+from cli2ansible.application.dtos.session_dto import SessionCreateRequestDTO, SessionResponseDTO
 
 __all__ = [
     # Session DTOs
